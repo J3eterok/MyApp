ab2f69ad97cedde8c06b41c6998afe34df292c96
Oliger Maksim <oligerno@gmail.com>
2018-04-17 02:12:45 +0300

    Merge remote-tracking branch 'v1.0/master'
    # Conflicts:
    #	app/src/main/java/co/example/user/myapp/Card_Search_View.java
    #	app/src/main/java/co/example/user/myapp/Offer.java

---
+++ app/src/main/java/co/example/user/myapp/CARD_Adapter.java
@@ -0,0 +1,64 @@
+package co.example.user.myapp;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/*Адаптер для RecyclerView (Карточки с мероприятиями)*/
+
+public class CARD_Adapter extends RecyclerView.Adapter<CARD_Adapter.PersonViewHolder>{
+
+    public static  class PersonViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView eventName;
+        TextView eventTime;
+        TextView eventAdress;
+        TextView eventCategory;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cv = itemView.findViewById(R.id.cv);
+            eventName = itemView.findViewById(R.id.eventName);
+            eventTime = itemView.findViewById(R.id.eventTime);
+            eventAdress = itemView.findViewById(R.id.eventAdress);
+            eventCategory = itemView.findViewById(R.id.eventCategory);
+        }
+    }
+
+    List<Card_Search_View.Event> events;
+
+    CARD_Adapter(List<Card_Search_View.Event> events){
+        this.events = events;
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cv, viewGroup, false);
+        PersonViewHolder pvh = new PersonViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+        personViewHolder.eventName.setText(events.get(i).name);
+        personViewHolder.eventTime.setText(events.get(i).time);
+        personViewHolder.eventAdress.setText(events.get(i).adress);
+        personViewHolder.eventCategory.setText(events.get(i).category);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+}
--- app/src/main/java/co/example/user/myapp/Card_Search_View.java
+++ app/src/main/java/co/example/user/myapp/Card_Search_View.java
@@ -1,73 +1,66 @@
 package co.example.user.myapp;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
 
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class Card_Search_View extends AppCompatActivity {
 
-    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
-    int matchParent = LinearLayout.LayoutParams.MATCH_PARENT;
+    public class Event { // Класс, описывающий событие
+        String name, time, adress, category;
+
+        Event(String name, String time, String adress, String category) {
+            this.name = name;
+            this.time = time;
+            this.adress = adress;
+            this.category = category;
+        }
+    }
+
+    private List<Event> events;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card__search__view);
-        //ToDo: Реализовать запрос с целью получения данных о мероприятии(Название, время, место, категория)
-        String name_text = "Название мероприятия";
-        String time_text = "Время";
-        String adress_text = "Адрес";
-        String category_text = "Категория";
-        LinearLayout Main = findViewById(R.id.main_content);
 
+        RecyclerView cards = (RecyclerView)findViewById(R.id.rv);
+        LinearLayoutManager mngr = new LinearLayoutManager(this);
+        cards.setLayoutManager(mngr);
+
+        events = new ArrayList<>();
+
+        int n=6;//кол-во мероприятий
+
+        //ToDo: Реализовать запрос с целью получения данных о мероприятии(Название, время, место, категория)
+
+        for(int i=0; i<n;i++)
+        {
+            events.add(new Event("Playing bingo with my grandpa", "1.06.2018", "Kazan", "Intelligence games"));// Подставить параметры мероприятия
+        }
+
+        CARD_Adapter adapter = new CARD_Adapter(events);
+        cards.setAdapter(adapter);
 
         String json = getIntent().getStringExtra("json");
         Gson gson = new Gson();
-        MyEvent[] events = gson.fromJson(json, MyEvent[].class);
+        //MyEvent[] events = gson.fromJson(json, MyEvent[].class);
 //        for(MyEvent event:events) {
-//            LinearLayout.LayoutParams Event_Layout = new LinearLayout.LayoutParams(wrapContent, matchParent);
-//            Event_Layout.gravity = Gravity.CENTER;
 //
-//
-//            //LinearLayout.LayoutParams Event_inner = new LinearLayout.LayoutParams(wrapContent, wrapContent);
-//
-//            TextView name = new TextView(this);
-//            name.setText(event.name);
-//            name.setTextColor(Color.parseColor("#000000"));
-//            name.setBackgroundResource(R.drawable.border);
-//            name.setOnClickListener(OpenEvent);
-//            Main.addView(name, Event_Layout);
-//
-//            TextView time = new TextView(this);
-//            time.setText(event.datetime);
-//            time.setTextColor(Color.parseColor("#000000"));
-//            Main.addView(time, Event_Layout);
-//
-//            TextView adress = new TextView(this);
-//            adress.setText(event.city);
-//            adress.setTextColor(Color.parseColor("#000000"));
-//            Main.addView(adress, Event_Layout);
-//
-//            TextView category = new TextView(this);
-//            category.setText(event.comment);
-//            category.setTextColor(Color.parseColor("#000000"));
-//            category.setPadding(0, 0, 0, 30);
-//            Main.addView(category, Event_Layout);
 //        }
     }
 
-    View.OnClickListener OpenEvent = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //ToDO: открытие мероприятия
-        }
-    };
+    public void ShowEvent(View view) //Метод открытия карточки
+    {
+        Intent intent = new Intent(this, EventView_Subscriber.class);
+        startActivity(intent);
+    }
 }
